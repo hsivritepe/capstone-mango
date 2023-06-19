819878115354d62a469472fddc3bfee67f22bc8a
hsivritepe <hakan@sivritepe.com>
2023-06-19 13:01:59 -0700

    Create styling and page layout

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -6,6 +7,13 @@ export default function Home() {
             <div className="p-12 flex items-center flex-col gap-6 justify-center text-3xl font-bold">
                 <h1>Mango</h1>
                 <h2 className="text-lg">Next.js + Tailwind CSS</h2>
+            </div>
+            <div className="flex justify-center items-center">
+                <Link href="/mango/dashboard">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Dashboard
+                    </button>
+                </Link>
             </div>
         </>
     );
