fd11ebec3610908c261b04683b0e5bcf47346899
hsivritepe <hakan@sivritepe.com>
2023-07-06 22:35:24 +0300

    Test URL

--- app/mango/dashboard/page.jsx
+++ app/mango/dashboard/page.jsx
@@ -66,6 +66,7 @@ export default function Dashboard() {
                 title="Dashboard"
                 description="Here you can see the houses (villas) listed that are in the system."
             />
+            TEST : {process.env.API_PATH}
             <section className="flex gap-10 bg-gray-500 p-6">
                 <Card className="flex-1">
                     <div className="flex justify-between">
@@ -139,109 +140,5 @@ export default function Dashboard() {
             </section>
             <RecentBookings />
         </>
-        // <>
-        //     <div class="row y-gap-30">
-        //         <div class="col-xl-3 col-md-6">
-        //             <div class="py-30 px-30 rounded-4 bg-white shadow-3">
-        //                 <div class="row y-gap-20 justify-between items-center">
-        //                     <div class="col-auto">
-        //                         <div class="fw-500 lh-14">
-        //                             Pending
-        //                         </div>
-        //                         <div class="text-26 lh-16 fw-600 mt-5">
-        //                             $12,800
-        //                         </div>
-        //                         <div class="text-15 lh-14 text-light-1 mt-5">
-        //                             Total pending
-        //                         </div>
-        //                     </div>
-        //                     <div class="col-auto">
-        //                         <Image
-        //                             src="/img/dashboard/1.svg"
-        //                             alt="Landscape picture"
-        //                             width={60}
-        //                             height={60}
-        //                         />
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <div class="col-xl-3 col-md-6">
-        //             <div class="py-30 px-30 rounded-4 bg-white shadow-3">
-        //                 <div class="row y-gap-20 justify-between items-center">
-        //                     <div class="col-auto">
-        //                         <div class="fw-500 lh-14">
-        //                             Earnings
-        //                         </div>
-        //                         <div class="text-26 lh-16 fw-600 mt-5">
-        //                             $14,200
-        //                         </div>
-        //                         <div class="text-15 lh-14 text-light-1 mt-5">
-        //                             Total earnings
-        //                         </div>
-        //                     </div>
-        //                     <div class="col-auto">
-        //                         <Image
-        //                             src="/img/dashboard/2.svg"
-        //                             alt="Landscape picture"
-        //                             width={60}
-        //                             height={60}
-        //                         />
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <div class="col-xl-3 col-md-6">
-        //             <div class="py-30 px-30 rounded-4 bg-white shadow-3">
-        //                 <div class="row y-gap-20 justify-between items-center">
-        //                     <div class="col-auto">
-        //                         <div class="fw-500 lh-14">
-        //                             Bookings
-        //                         </div>
-        //                         <div class="text-26 lh-16 fw-600 mt-5">
-        //                             $8,100
-        //                         </div>
-        //                         <div class="text-15 lh-14 text-light-1 mt-5">
-        //                             Total hotel bookings
-        //                         </div>
-        //                     </div>
-        //                     <div class="col-auto">
-        //                         <Image
-        //                             src="/img/dashboard/3.svg"
-        //                             alt="Landscape picture"
-        //                             width={60}
-        //                             height={60}
-        //                         />
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //         <div class="col-xl-3 col-md-6">
-        //             <div class="py-30 px-30 rounded-4 bg-white shadow-3">
-        //                 <div class="row y-gap-20 justify-between items-center">
-        //                     <div class="col-auto">
-        //                         <div class="fw-500 lh-14">
-        //                             Services
-        //                         </div>
-        //                         <div class="text-26 lh-16 fw-600 mt-5">
-        //                             22,786
-        //                         </div>
-        //                         <div class="text-15 lh-14 text-light-1 mt-5">
-        //                             Total bookable services
-        //                         </div>
-        //                     </div>
-        //                     <div class="col-auto">
-        //                         <Image
-        //                             src="/img/dashboard/4.svg"
-        //                             alt="Landscape picture"
-        //                             width={60}
-        //                             height={60}
-        //                         />
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </>
     );
 }
