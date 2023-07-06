f34b344a735c8fb260ed414c6f71143179d26559
hsivritepe <hakan@sivritepe.com>
2023-07-06 22:27:56 +0300

    Set config for API URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_PATH: 'http://localhost:5050/api/v1/',
+        // API_PATH: 'http://localhost:5050/api/v1/',
+        API_PATH: 'us-cdbr-east-06.cleardb.net:47131/api/v1/',
     },
 };
 
