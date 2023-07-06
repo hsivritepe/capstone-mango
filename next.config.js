95ff3caa8aaf90d84051c8d32b0793d1c3c98d27
hsivritepe <hakan@sivritepe.com>
2023-07-06 22:39:49 +0300

    Adjust API URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     env: {
         // API_PATH: 'http://localhost:5050/api/v1/',
-        API_PATH: 'us-cdbr-east-06.cleardb.net:47131/api/v1/',
+        API_PATH:
+            'https://hakan-capstone-mango-api-44eaf68d7b10.herokuapp.com/api/v1/',
     },
 };
 
